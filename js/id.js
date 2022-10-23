e9137abfcdb2b5771fa8beb6f69de38fd0766e9f
dayanjr <dayanfrazao@byui.edu>
2022-10-22 21:30:09 -0600

    up

--- js/id.js
+++ js/id.js
@@ -1,6 +1,37 @@
-let date = new Date();
-let currentdate = document.lastModified;
-document.querySelector('#omed').textContent = currentdate;
+const today = new Date();
+let dayOfWeek;
+dayOfWeek = today.getDay();
+if (dayOfWeek == 0){
+    dayOfWeek = "sunday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 1){
+    dayOfWeek = "monday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 2){
+    dayOfWeek = "tuesday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 3){
+    dayOfWeek = "wendnesday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 4){
+    dayOfWeek = "thursday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 5){
+    dayOfWeek = "friday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}else if (dayOfWeek == 6){
+    dayOfWeek = "saturday";
+    document.getElementById("omed").textContent = dayOfWeek;
+}
+else{
+    console.log("error");
+}
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-
-
+const d = new Date();
+let month = months[d.getMonth()];
+document.getElementById("om").innerHTML = month;
+const day = new Date();
+document.getElementById("o").innerHTML = day.getDate();
+const y = new Date();
+document.getElementById("year").innerHTML = y.getFullYear();
---
+++ merda.py
@@ -0,0 +1,10 @@
+A1 = 'CON'
+B1 = 'D'
+sce = input("asdda:")
+if sce == A1.lower():
+    input("youmove")
+elif sce == B1.lower():
+    input("duck")
+else:
+    print("plesse")
+print("")
